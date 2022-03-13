2983795554a625c061797ca2a3ed25143028a578
Fatih Kurt <lifelonglearnerest@gmail.com>
2022-03-13 18:39:17 +0300

    path fixes for middleware

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ var app = express()
 const absoluteViewIndexHtmlPath = __dirname + "/views/index.html"
 const absoluteStyleSheetPath = __dirname + "/public"
 
-app.use(express.static(absoluteStyleSheetPath))
+app.use("/public", express.static(absoluteStyleSheetPath))
 
 app.get("/", (req, res) => {
   res.sendFile(absoluteViewIndexHtmlPath)
