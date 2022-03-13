79b28392b639957e37024e95b45d0579a7f196c6
Fatih Kurt <lifelonglearnerest@gmail.com>
2022-03-13 18:19:49 +0300

    res send file index html path

--- myApp.js
+++ myApp.js
@@ -1,8 +1,11 @@
 var express = require("express")
 var app = express()
 
+const absoluteViewIndexHtmlPath = __dirname + "/views/index.html"
+
+console.log("dirname", path)
 app.get("/", (req, res) => {
-  res.send("Hello Express")
+  res.sendFile(absoluteViewIndexHtmlPath)
 })
 
 module.exports = app
