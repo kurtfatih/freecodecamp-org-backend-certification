58711ddc63af5deb658ea91ac8045ce1ea95a686
Fatih Kurt <lifelonglearnerest@gmail.com>
2022-03-13 18:35:13 +0300

    serve express static stylesheet

--- myApp.js
+++ myApp.js
@@ -2,6 +2,9 @@ var express = require("express")
 var app = express()
 
 const absoluteViewIndexHtmlPath = __dirname + "/views/index.html"
+const absoluteStyleSheetPath = __dirname + "/public"
+
+app.use(express.static(absoluteStyleSheetPath))
 
 app.get("/", (req, res) => {
   res.sendFile(absoluteViewIndexHtmlPath)
