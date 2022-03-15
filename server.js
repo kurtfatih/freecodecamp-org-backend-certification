68dec50091e024377e35d7ae621ed9f7c0585e54
Fatih Kurt <lifelonglearnerest@gmail.com>
2022-03-15 12:35:21 +0300

    port changed

--- server.js
+++ server.js
@@ -66,6 +66,6 @@ app.get("/api/hello", function (req, res) {
 })
 
 // listen for requests :)
-var listener = app.listen(8080, function () {
+var listener = app.listen(process.env.PORT || 8000, function () {
   console.log("Your app is listening on port " + listener.address().port)
 })
